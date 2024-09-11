6c0301baf42a3ea28adc7bd6f3652e38b3a546cf
ali astereki <alibaba2a1999@gmail.com>
2024-09-11 11:53:31 +0200

    create header & set bg to body

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["public/**/*.{html, js}"],
+  darkMode: 'class',
   theme: {
     extend: {
 
@@ -8,6 +9,27 @@ module.exports = {
         "morabaLight": 'morabaLight',
         "morabaMedium": 'morabaMedium',
         "morabaBold": 'morabaBold ',
+      },
+
+      borderRadius: {
+        "4xl": "2rem"
+      },
+
+      colors: {
+        "brown": {
+          100: "#ECE0D1",
+          300: "#D8C1AC",
+          600: "#967259",
+          900: "#634832",
+        }
+      },
+
+      boxShadow: {
+        "normal": "0px 1px 10px rgba(0,0,0, 0.5)",
+      },
+
+      letterSpacing:{
+        "tightest": "-0.068em"
       }
 
     },
