6dcbcd9ee250b5866f60083ccd58e64e7a2d1359
ali astereki <alibaba2a1999@gmail.com>
2024-09-14 15:03:20 +0200

    start mobile menu

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,32 @@
-const themeToggleBtn = document.querySelector('#theme-toggle')
-themeToggleBtn.addEventListener('click', e=>{
-    if(localStorage.theme === 'dark'){
-        document.documentElement.classList.remove("dark")
-        localStorage.theme = "light"
-    }else{
-         document.documentElement.classList.add("dark")
-         localStorage.setItem("theme", "dark")
-    }
+const themeToggleBtns = document.querySelectorAll('.theme-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+const mobileMenuBtn = document.getElementById('menu-toggler');
+const closeMobileMenuBtn = document.getElementById('close-mobile-menu');
+const coverMobileMenu = document.getElementById('overlay');
+
+themeToggleBtns.forEach(function(themeToggleBtn){
+
+    themeToggleBtn.addEventListener('click', e=>{
+        if(localStorage.theme === 'dark'){
+            document.documentElement.classList.remove("dark")
+            localStorage.theme = "light"
+        }else{
+             document.documentElement.classList.add("dark")
+             localStorage.setItem("theme", "dark") 
+        }
+    })
+})
+
+
+
+const closeMobileMenu = ()=>{
+    mobileMenu.classList.replace("right-0","-right-60")
+    coverMobileMenu.classList.replace('visible', 'hidden') 
+}
+closeMobileMenuBtn.addEventListener('click', closeMobileMenu)
+coverMobileMenu.addEventListener('click', closeMobileMenu)
+
+mobileMenuBtn.addEventListener("click", e=>{
+    mobileMenu.classList.replace("-right-60", "right-0")
+    coverMobileMenu.classList.replace('hidden', 'visible') 
 })
